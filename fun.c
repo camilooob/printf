9b2822e2912f86b9b30787538bd8bfa0f0bcae3e
camilooob <camilobaq@hotmail.com>
2019-10-25 18:09:30 -0500

    try

--- fun.c
+++ fun.c
@@ -7,6 +7,29 @@
  **/
 int _printf(const char *format, ...)
 {
+	va_list ap;
+	unsigned int i;
+	char *str;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = (va_arg(ap, char *));
+		if (str != NULL)
+		{
+				printf("%s", str);}
+				else
+				{
+				printf("(nil)");
+				}
+
+			if ((i != (n - 1)) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(ap);
 
 
 
