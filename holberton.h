71323db1e5a23184061eb538069cac81bdb6e2f9
cbayonao <drcamilob@live.com>
2019-10-26 10:07:18 -0500

    [Feature 1] Adding the estructure for printf proj.

--- holberton.h
+++ holberton.h
@@ -1,4 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 int _printf(const char *format, ...);
+typedef struct select
+{
+	char c;
+	void (*f)(va_list listed);
+}typarg;
 #endif
