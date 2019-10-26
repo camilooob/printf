65820bc3785bd9bbec4bbc8326b3e278ee43db14
camilooob <camilobaq@hotmail.com>
2019-10-25 19:31:46 -0500

    draft

--- driver.c
+++ driver.c
@@ -4,18 +4,8 @@
  * @format: string that contains the specifier
  * Return: pointer to the print function
  */
-int (*get_print_fn(char *format))(char *format, va_list)
+int (*get_print_fn(char *format))
 {
-	int i;
 
-	print_fn getfn[] = {
-		{"%c", p_char},
-		{"%s", p_str},
-		{NULL, NULL}
-	};
-	for (i = 0; getfn[i].specifier; i++)
-	{
-		if (format[1] == getfn[i].specifier[1])
-			return (getfn[i].fn);
-	}
+
 }
