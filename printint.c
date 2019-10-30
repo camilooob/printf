b3713b9fa19114475e5568597e84eb34b4f68c56
camilooob <camilobaq@hotmail.com>
2019-10-29 19:20:14 -0500

    fix betty

--- printint.c
+++ printint.c
@@ -43,11 +43,11 @@ int integer(int number)
 	int count;
 
 	count = contadordigit(number);
-	if (number < 0)
-	{
-		 _putchar('-');
-		unint = -number;
-	}
+if (number < 0)
+{
+_putchar('-');
+unint = -number;
+}
 	else
 		unint = number;
 
